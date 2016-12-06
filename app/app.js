74aa1cb3969d230040e00f0f777a51de9e057820
asimonia <alex.simonian@gmail.com>
2016-12-06 13:26:06 -0600

    Added app.js

---
+++ app/app.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var app = angular.module("PinterestApp", ["ngRoute", "ngMaterial"]);
+
+let isAuth = (AuthFactory) => new Promise( (resolve, reject) => {
+  AuthFactory.isAuthenticated()
+  .then( (userExists) => {
+    if(userExists) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+});
+
+app.config(function($routeProvider, $mdThemingProvider) {
+  $routeProvider.
+  when('/login', {
+    templateUrl: 'partials/login.html',
+    controller: 'LoginCtrl'
+  }).
+  when('/board', {
+    templateUrl: 'partials/add-board.html',
+    controller: 'AddBoardCtrl'
+  }).
+  when('/pin', {
+    templateUrl: 'partials/add-pin.html',
+    controller: 'AddPinCtrl'
+  }).
+  when('/view', {
+    templateUrl: 'partials/view-board.html',
+    controller: 'ViewBoardCtrl'
+  }).
+  otherwise('/');
+  
+  $mdThemingProvider.theme('default')
+    .primaryPalette('teal')
+    .accentPalette('orange');
+});
+
+app.run( ($location, FBCreds) => {
+  let creds = FBCreds;
+  let authConfig = {
+    apiKey: creds.key,
+    authDomain: creds.authDomain
+  };
+  firebase.initializeApp(authConfig);
+});
+
+
