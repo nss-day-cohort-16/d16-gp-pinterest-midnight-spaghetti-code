821fb8383597c59ddb54ed0ee26aba77633cf520
Matthew McCord <mattm.eternal@gmail.com>
2016-12-06 11:56:07 -0600

    working on cool guy master branch. auth factory

---
+++ app/factories/AuthFactory.js
@@ -0,0 +1,38 @@
+"use strict";
+
+app.factory("AuthFactory", function() {
+
+	let currentUser = null,
+			provider = new firebase.auth.GoogleAuthProvider();
+
+	let AuthObject = {
+		// createUser(userObj) {
+		// 	return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password);
+		// },
+		loginUser(userObj) {
+			currentUser = null;
+	    return firebase.auth().signInWithPopup(provider); 
+		},
+		logoutUser(userObj) {
+			return firebase.auth().signOut();
+		},
+		isAuthenticated() {
+			return new Promise((resolve, reject) => {
+				firebase.auth().onAuthStateChanged((user) => {
+					if (user) {
+						currentUser = user.uid;
+						resolve(true);
+					} else {
+						resolve(false);
+					}
+				});
+			});
+		},
+		getUser() {
+			return currentUser;
+		}
+	};
+
+	return AuthObject;
+
+});
