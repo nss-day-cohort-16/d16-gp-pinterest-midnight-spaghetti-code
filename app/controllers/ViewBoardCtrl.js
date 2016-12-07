03b62f6cbe0525690e1dd205e483c3299021cb3b
asimonia <alex.simonian@gmail.com>
2016-12-07 14:12:33 -0600

    ViewBoard and pin functionality

--- app/controllers/ViewBoardCtrl.js
+++ app/controllers/ViewBoardCtrl.js
@@ -1,32 +1,20 @@
 "use strict";
 
-app.controller("ViewBoardCtrl", function($scope) {
+app.controller("ViewBoardCtrl", function($scope, DataFactory) {
+	$scope.hasPins = false;
+	$scope.pins = [];
 
-	$scope.pins = [
-	{
-		"title": "Monkey Scene",
-		"image": "http://i.imgur.com/iK6GHHt.jpg"
-	},
-	{
-		"title": "Raymond's Mom",
-		"image": "http://yts.ac/wp-content/uploads/tt0456554-large-screenshot2.jpg"
-	},
-	{
-		"title": "Adios Turdnuggets",
-		"image": "http://i.imgur.com/0fscX95.gif"
-	},
-	{
-		"title": "Monkey Scene",
-		"image": "http://i.imgur.com/iK6GHHt.jpg"
-	},
-	{
-		"title": "Raymond's Mom",
-		"image": "http://yts.ac/wp-content/uploads/tt0456554-large-screenshot2.jpg"
-	},
-	{
-		"title": "Adios Turdnuggets",
-		"image": "http://i.imgur.com/0fscX95.gif"
-	}
-	];
-
+	DataFactory.getBoards()
+	  .then((data) => {
+	  	$scope.boardname = data.name;
+	  	DataFactory.getPins()
+	  	.then((data) => {
+	  		$scope.pins = data;
+	  		if ($scope.pins.length > 0)
+	  			$scope.hasPins = true;
+	  		else
+	  			$scope.hasPins = false;
+	  		$scope.$apply();
+	  	})
+	  });
 });
