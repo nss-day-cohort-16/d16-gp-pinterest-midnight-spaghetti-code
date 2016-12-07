0c538e88b79891773922daa314a136d172b88334
asimonia <alex.simonian@gmail.com>
2016-12-07 12:24:28 -0600

    Added cards

--- app/controllers/ViewBoardCtrl.js
+++ app/controllers/ViewBoardCtrl.js
@@ -1,9 +1,32 @@
 "use strict";
 
-app.controller("ViewBoardCtrl", function($scope){
+app.controller("ViewBoardCtrl", function($scope) {
 
-		function genericFunction() {
-
-		}
+	$scope.pins = [
+	{
+		"title": "Monkey Scene",
+		"image": "http://i.imgur.com/iK6GHHt.jpg"
+	},
+	{
+		"title": "Raymond's Mom",
+		"image": "http://yts.ac/wp-content/uploads/tt0456554-large-screenshot2.jpg"
+	},
+	{
+		"title": "Adios Turdnuggets",
+		"image": "http://i.imgur.com/0fscX95.gif"
+	},
+	{
+		"title": "Monkey Scene",
+		"image": "http://i.imgur.com/iK6GHHt.jpg"
+	},
+	{
+		"title": "Raymond's Mom",
+		"image": "http://yts.ac/wp-content/uploads/tt0456554-large-screenshot2.jpg"
+	},
+	{
+		"title": "Adios Turdnuggets",
+		"image": "http://i.imgur.com/0fscX95.gif"
+	}
+	];
 
 });
